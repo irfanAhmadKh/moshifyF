ecf39f7696d4548ce4b65bed89fd1874158ce761
Irfan Ahmad <qureshi3254@gmail.com>
2025-12-16 11:51:03 +0500

    Lists - Side Ticks

--- dist/normalize.1114379f.js
+++ dist/normalize.1114379f.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51298" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58739" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
