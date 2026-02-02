77d6dfb6c410876b33ed1adea8aa809292bc85b2
Irfan Ahmad <qureshi3254@gmail.com>
2026-02-02 13:13:39 +0500

    Cards + plans

--- dist/styles.b61e60ae.js
+++ dist/styles.b61e60ae.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\tick.svg":[["tick.2a1a279a.svg","images/tick.svg"],"images/tick.svg"],"_css_loader":"C:/Users/Irfan Ahmad/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/Irfan Ahmad/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\tick.svg":[["tick.2a1a279a.svg","images/tick.svg"],"images/tick.svg"],"./..\\images\\popular.svg":[["popular.ef67f06e.svg","images/popular.svg"],"images/popular.svg"],"_css_loader":"C:/Users/Irfan Ahmad/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/Irfan Ahmad/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56124" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59847" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
